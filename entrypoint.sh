33cbd0d97960bb776569ae454bb32bf7a7900b4a
Craig Edmunds <craig.edmunds@gmail.com>
2024-11-15 11:26:38 +0000

    Fixing docker setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,9 +17,10 @@ mkdir -p ${JM_REPORTS} ${JM_LOGS}
 SCENARIOFILE=${JM_SCENARIOS}/${TEST_SCENARIO}.jmx
 REPORTFILE=${NOW}-perftest-${TEST_SCENARIO}-report.csv
 LOGFILE=${JM_LOGS}/perftest-${TEST_SCENARIO}.log
+PROPERTIESFILE=${JM_HOME}/jmeter.properties
 
 # Run the test suite
-jmeter -n -t ${SCENARIOFILE} -e -l "${REPORTFILE}" -o ${JM_REPORTS} -j ${LOGFILE} -f -Jenv="${ENVIRONMENT}" -JTimespan="${JMETER_TIMESPAN:-All}" -JHost="${JMETER_HOST:=cdms-backend.perf-test.cdp-int.defra.cloud}" -JProtocol="${JMETER_Protocol:-https}" -JPort="${JMETER_PORT:-443}"
+jmeter -n -t ${SCENARIOFILE} -p "${PROPERTIESFILE}" -e -l "${REPORTFILE}" -o ${JM_REPORTS} -j ${LOGFILE} -f -Jenv="${ENVIRONMENT}" -JTimespan="${JMETER_TIMESPAN:-All}" -JHost="${JMETER_HOST:=cdms-backend.perf-test.cdp-int.defra.cloud}" -JProtocol="${JMETER_Protocol:-https}" -JPort="${JMETER_PORT:-443}"
 test_exit_code=$?
 
 # Publish the results into S3 so they can be displayed in the CDP Portal
